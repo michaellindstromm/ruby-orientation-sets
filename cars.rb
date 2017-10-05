b4b94888e62f8cb9a4cf080b3462646555a46c29
Michael Lindstrom <michaellindstr@gmail.com>
2017-10-05 14:03:48 -0500

    finished

---
+++ cars.rb
@@ -0,0 +1,28 @@
+require "set"
+showroom = Set.new
+showroom << 'Accord' << 'Fit' << 'Nugget' << 'Tuson'
+
+puts showroom.length
+
+showroom << 'Accord'
+
+puts showroom.inspect
+
+new_cars = Set.new ['Monster Truck', 'Big Rig']
+showroom.add(new_cars)
+
+puts showroom.inspect
+
+showroom.delete('Fit')
+
+puts showroom.inspect
+
+junkyard = Set.new
+
+junkyard << 'Nugget' << 'Tuson' << 'Smart' << 'R8' << 'Monster Truck'
+
+puts showroom.inspect
+puts junkyard.inspect
+
+new_set = showroom | junkyard
+puts new_set.inspect
